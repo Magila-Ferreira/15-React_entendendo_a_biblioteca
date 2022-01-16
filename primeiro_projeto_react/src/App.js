76070d818969a089ebf1eabcdf8b3f715574b178
Mágila-Ferreira <magilamorganasf@hotmail.com>
2022-01-16 10:14:37 -0300

    16/01/22_Aula 4 - Lidando com eventos e propriedades

--- primeiro_projeto_react/src/App.js
+++ primeiro_projeto_react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
+import "./assets/App.css";
+import "./assets/index.css";
 
 class App extends Component {
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
@@ -15,4 +17,8 @@ class App extends Component {
 
 export default App;
 
-// ***** RODAR OS COMANDOS GIT's dentro da pasta_terminal: Y:\DS\15-React_entendendo_a_biblioteca\primeiro_projeto_react
+// ***** RODAR OS COMANDOS GIT's dentro da pasta_terminal: 
+// Z:\DS\15-React_entendendo_a_biblioteca
+
+// ***** RODAR O COMANDO "NPM SATART" dentro da pasta_terminal:  
+// Z:\15-React_entendendo_a_biblioteca\primeiro_projeto_react
